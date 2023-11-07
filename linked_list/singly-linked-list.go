63f7025024649f8647cb3d6b1df719a1b4d5749d
Chesnokov Arkady <arkady-chesnokof@yandex.ru>
2023-11-07 15:25:38 +0300

    add AddAtBeg()

--- linked_list/singly-linked-list.go
+++ linked_list/singly-linked-list.go
@@ -2,5 +2,16 @@ package linked_list
 
 type Singly[T any] struct {
 	length int
-	Head   Node[T]
+	Head   *Node[T]
+}
+
+func NewSingly[T any]() *Singly[T] {
+	return &Singly[T]{}
+}
+
+func (sl *Singly[T]) AddAtBeg(val T) {
+	node := NewNode(val)
+	node.next = sl.Head
+	sl.Head = node
+	sl.length++
 }
