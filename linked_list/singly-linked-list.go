126e7b6bff041bad1dc910aae7e2eaaf138efd67
Chesnokov Arkady <arkady-chesnokof@yandex.ru>
2023-11-08 11:36:56 +0300

    add AddAtEnd() & DeleteAtBeg() for singly linked list

--- linked_list/singly-linked-list.go
+++ linked_list/singly-linked-list.go
@@ -15,3 +15,36 @@ func (sl *Singly[T]) AddAtBeg(val T) {
 	sl.Head = node
 	sl.length++
 }
+
+func (sl *Singly[T]) AddAtEnd(val T) {
+	node := NewNode(val)
+	head := sl.Head
+
+	if head != nil {
+
+		for head.next != nil {
+			head = head.next
+		}
+
+		head.next = node
+	} else {
+		sl.Head = node
+	}
+
+	sl.length++
+}
+
+// DeleteAtBeg delete head node
+// return its value and false if list was empty
+func (sl *Singly[T]) DeleteAtBeg() (T, bool) {
+	if sl.Head == nil {
+		var r T
+		return r, false
+	}
+
+	cur := sl.Head
+	sl.Head = cur.next
+	sl.length--
+
+	return cur.info, true
+}
